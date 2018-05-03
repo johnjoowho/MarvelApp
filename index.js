7aec9bb1037d0ea34de4adeccf2dd3164fc3d0e9
John Joo <john@Johns-iMac.local>
2018-05-02 17:59:47 -0700

    testing console.log(json.stringify(data))

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function renderResults() {
 } 
 
 function displayMarvelData(data) { 
-  console.log(Object.keys(data.data.results)); 
+  console.log(JSON.stringify(data)); 
 }
 
 function watchSubmit() {
