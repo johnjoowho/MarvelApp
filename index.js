fb0ce9821c16e49d139a5188e8289dd3b67b5326
John Joo <john@Johns-iMac.local>
2018-05-05 14:57:07 -0700

    testing data.data.results in DOM

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ function getDataFromApi(searchTerm, callback) {
 
 function renderResults(data) {
   return `
-  <h1>Character name: ${data.results[0].name}</h1> 
-  <p>description: ${data.results[0].description}</p> 
-  <img src="${data.results[0].thumbnail.path}/portrait_xlarge.${data.results[0].thumbnail.extension}">
+  <h1>Character name: ${data.data.results[0].name}</h1> 
+  <p>description: ${data.data.results[0].description}</p> 
+  <img src="${data.data.results[0].thumbnail.path}/portrait_xlarge.${data.results[0].thumbnail.extension}">
   `
 } 
 
