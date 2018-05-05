b0072400dc677c357d2211bb9137feff03f0fde0
John Joo <john@Johns-iMac.local>
2018-05-05 15:27:57 -0700

    removed unncessary comic info

--- index.js
+++ index.js
@@ -21,10 +21,7 @@ function renderResults(data) {
   
   <img src="${data.data.results[0].thumbnail.path}/portrait_xlarge.${data.data.results[0].thumbnail.extension}">
   <p class="character-description">Description: ${data.data.results[0].description}</p>
-  <a href="${data.data.results[0].urls[0].url}">Link to comics ${data.data.results[0].name} is featured in</a> 
-  <p class="character-events">Number of comics available: ${data.data.results[0].comics.available}</p> 
-  <p class="character-events">Number of comics returned: ${data.data.results[0].comics.returned}</p>
-  <a href="${data.data.results[0].comics.collectionURI}">Link to Collection URI</a>
+  <a href="${data.data.results[0].urls[0].url}">Link to comics ${data.data.results[0].name} apeears in</a> 
   `
 
   
