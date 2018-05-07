716b786119d684d04dfe2a7fd6918d02f3814669
John Joo <john@Johns-iMac.local>
2018-05-07 15:24:52 -0700

    line 56

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function renderResults(data) {
 
 function renderResults2(data) { 
   return `
-  <p class="movieoverview">${data.object.results[0]}</p>
+  <p class="movieoverview">${object.results[0]}</p>
   `
 }
 
